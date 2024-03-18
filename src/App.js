010f34dff26e6e57783e3be5115af33ccd85f28d
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-18 12:33:30 +0530

    add header comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Header from "./Component/Header/Header";
 import Navbar from "./Component/Navbar/Navbar";
-
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Header/>
     </div>
   );
 }
