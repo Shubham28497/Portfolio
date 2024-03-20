bfe613de0cd508995d47bc40488015cebd70d0e3
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-20 09:31:26 +0530

    add service comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Component/Header/Header";
 import Navbar from "./Component/Navbar/Navbar";
+import Services from "./Component/Services/Services";
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Header/>
+    <Services/>
     </div>
   );
 }
