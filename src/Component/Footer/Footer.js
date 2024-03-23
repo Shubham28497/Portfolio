f6de29743bf18a6e4a8575fd9775c87c09d8c15e
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-03-23 21:30:06 +0530

    cmnt link

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -19,11 +19,11 @@ const Footer = () => {
             <i className="fa-brands fa-facebook-f"></i>
           </a>
         </li>
-        <li>
+        {/* <li>
           <a href="https://www.youtube.com/c/inovotek-academy">
             <i className="fa-brands fa-twitter"></i>
           </a>
-        </li>
+        </li> */}
         {/* <li>
           <a href="https://www.youtube.com/c/inovotek-academy">
             <i className="fa-brands fa-instagram"></i>
